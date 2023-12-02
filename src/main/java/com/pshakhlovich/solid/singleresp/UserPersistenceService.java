2fe7756cf844f69024666b10e945496ea11cf47f
Pavel Shakhlovich <altmersc@gmail.com>
2023-12-02 22:01:31 +0400

    Single responsibility principle

---
+++ src/main/java/com/pshakhlovich/solid/singleresp/UserPersistenceService.java
@@ -0,0 +1,11 @@
+package com.pshakhlovich.solid.singleresp;
+
+//A separate class for handling persistence 
+public class UserPersistenceService {
+
+	private Store store = new Store();
+	
+	public void saveUser(User user) {
+		store.store(user);
+	}
+}
